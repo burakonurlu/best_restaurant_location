284d1d1377077430498894f6041f67ef0441b0fb
Daniel Polistchuck <tchucky@gmail.com>
2022-09-09 17:26:27 +0300

    fixing errors

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"${tchucky@gmail.com}\"\n\
+email = \"tchucky@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS = false\n\
+enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
